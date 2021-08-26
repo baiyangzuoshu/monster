000c48838a51f15efc3e9020e21a25094b7964e1
baiyangzuoshu <baiyangzuo_shu@aliyun.com>
2021-08-26 13:06:47 +0800

    test:test

--- assets/scripts/bulletBase.js
+++ assets/scripts/bulletBase.js
@@ -1,9 +1,6 @@
-// Learn cc.Class:
-//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+/*
+zhengdekeyibianjima
+*/
 
 cc.Class({
     extends: cc.Component,
